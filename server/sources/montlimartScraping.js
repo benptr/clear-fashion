0c6a93d95c8dcdb458a4874ec86b5f050bb5aff6
benptr <benjamin.porterie@edu.devinci.fr>
2022-03-06 14:41:19 +0100

    Montlimart Scraping

--- server/sources/montlimartScraping.js
+++ server/sources/montlimartScraping.js
@@ -1,21 +1,22 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./montlimart');
-
-async function getDedicated (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+const montlimartbrand = require('./montlimart');
+var listProduct = []
+async function getMontlimart (eshop = 'https://www.montlimart.com/toute-la-collection.html?col=338&size=136') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await montlimartbrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
-    process.exit(0);
+    listProduct = products
+    return listProduct
   } catch (e) {
     console.error(e);
-    process.exit(1);
+
   }
 }
 
 const [,, eshop] = process.argv;
 
-getDedicated(eshop);
+getMontlimart(eshop);
